00c01055cebb053f12025a6bb6864066f09da690
Rosana <annros2003@gmail.com>
2024-02-05 10:08:39 -0800

    sprite locations overwritten

--- src/utils/SpriteLocation.h
+++ src/utils/SpriteLocation.h
@@ -2,11 +2,30 @@
 #define INC_2022_WIN64_SPRITELOCATION_H
 
 #include <filesystem>
+#include <string>
 
 class SpriteLocation {
 public:
-    static const std::filesystem::path doodleSpriteLocation;
-    static const std::filesystem::path platformSpriteLocation;
+    static const std::string getFolder();
+
+    static const std::filesystem::path getDoodleSpriteLocation();
+    static const std::filesystem::path getDoodleShootingSpriteLocation();
+
+    static const std::filesystem::path getPlatformSpriteLocation();
+
+    static const std::filesystem::path getProjectileSpriteLocation();
+
+    static const std::filesystem::path getButterflyEnemySpriteLocation();
+    static const std::filesystem::path getTallEnemySpriteLocation();
+
+    static const std::filesystem::path getJumperSpriteLocation();
+    static const std::filesystem::path getSpringSpriteLocation();
+    static const std::filesystem::path getJetpackSpriteLocation();
+
+    static const std::filesystem::path getPlayAgainButtonSpriteLocation();
+    static const std::filesystem::path getPlayButtonSpriteLocation();
+    static const std::filesystem::path getScoresButtonSpriteLocation();
+    static const std::filesystem::path getExitButtonSpriteLocation();
 
 };
 
